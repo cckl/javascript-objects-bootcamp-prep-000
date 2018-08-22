0f35d9c2cd015771601375063d9106b7f81f1950
Christina L <clingc7@gmail.com>
2018-08-22 23:43:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,12 +1,34 @@
-var playlist = { 
-  "Slowdive": "Alison",
-  "My Bloody Valentine": "Sometimes"
-}
+var playlist
 
-function updatePlaylist (playlist, artistName, songTitle) {
-  return Object.assign(playlist, { "Phil Ochs": ["Here's to the State of Mississipi"] } )
-}
 
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist.Slowdive;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var playlist = { 
+//   "Slowdive": "Alison",
+//   "My Bloody Valentine": "Sometimes"
+// }
+
+// function updatePlaylist (playlist, artistName, songTitle) {
+//   return Object.assign(playlist, { "Phil Ochs": ["Here's to the State of Mississipi"] } )
+// }
+
+// function removeFromPlaylist(playlist, artistName) {
+//   delete playlist.Slowdive
+//   return playlist
+// }
